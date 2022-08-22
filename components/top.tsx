c5b41b5a2589db2e9f2ee9b52296fbbc7338c670
岩本一希 <iwamotokazuki@iwamotokazukinoMacBook-Pro.local>
2022-08-22 16:01:17 +0900

    mod

--- components/top.tsx
+++ components/top.tsx
@@ -3,9 +3,7 @@
 const Top = () => {
     return (
       <div className="hero-container">
-        {/* 
         <video src="/Work2.mp4" autoPlay loop muted />
-        */}
       <h1 className="top-main-title">Welcome to EPknoT.</h1>
       </div>
   
